fd733c2dc1fbfe54b7d23eb90469af7efb13b047
Muhammad Zamroni <halo@matriphe.com>
2021-04-02 17:17:58 +0200

    Add larinfo config

---
+++ config/larinfo.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linfo Settings
+    |--------------------------------------------------------------------------
+    |
+    | This value is used to set the Linfo. You might not need to edit this.
+    |
+    */
+
+    'linfo' => [
+        'show' => [
+            'kernel' => true,
+            'os' => true,
+            'ram' => true,
+            'mounts' => true,
+            'webservice' => true,
+            'phpversion' => true,
+            'uptime' => true,
+            'cpu' => true,
+            'distro' => true,
+            'model' => true,
+            'virtualization' => true,
+            'duplicate_mounts' => false,
+            'mounts_options' => false,
+        ],
+    ],
+];
