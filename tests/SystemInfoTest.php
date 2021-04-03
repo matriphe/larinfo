4acdaaa2144bace17f84be391828b08edce50dbc
Muhammad Zamroni <halo@matriphe.com>
2021-04-03 13:59:45 +0200

    Add SystemInfo Test

---
+++ tests/SystemInfoTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Matriphe\Larinfo\Tests;
+
+use DavidePastore\Ipinfo\Ipinfo;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Http\Request;
+use Linfo\Linfo;
+use Matriphe\Larinfo\Entities\IpAddressChecker;
+use Matriphe\Larinfo\Larinfo;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class SystemInfoTest extends TestCase
+{
+    /**
+     * @var Larinfo
+     */
+    private $larinfo;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->larinfo = $larinfo = new Larinfo(
+            new Ipinfo(),
+            Request::capture(),
+            new Linfo(),
+            Mockery::mock(Manager::class),
+            new IpAddressChecker()
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+
+        parent::tearDown();
+    }
+
+    /**
+     * @group macos
+     */
+    public function testMacOsCatalina()
+    {
+        $this->assertEquals([
+            'os' => 'MacOS 10.15.7',
+            'distro' => '',
+            'kernel' => '19.6.0',
+            'arc' => 'x86_64',
+            'webserver' => 'Unknown',
+            'php' => '8.0.3',
+        ], $this->larinfo->getServerInfoSoftware());
+    }
+
+    /**
+     * @group ubuntu
+     */
+    public function testUbuntu()
+    {
+        $info = $this->larinfo->serverInfoSoftware();
+
+        $this->assertEquals('Linux', $info->getOS());
+        $this->assertEquals('Ubuntu 20.04.2 LTS', $info->getDistroString());
+        $this->assertEquals('5.10.13', $info->getKernel());
+        $this->assertEquals('x86_64', $info->getArch());
+    }
+}
