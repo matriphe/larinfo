d44d730aca6527b7dab1d4d7f1c8903b57173c1b
Muhammad Zamroni <halo@matriphe.com>
2021-04-03 23:52:58 +0200

    Add Wrapper factory

---
+++ src/Wrapper/WrapperFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Matriphe\Larinfo\Wrapper;
+
+use Linfo\Exceptions\FatalException;
+use Linfo\Linfo;
+use Matriphe\Larinfo\Windows\WindowsOs;
+use Matriphe\Larinfo\Windows\WindowsWrapper;
+
+class WrapperFactory
+{
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * WrapperFactory constructor.
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @return LinfoWrapperContract
+     */
+    public function getWrapper(): LinfoWrapperContract
+    {
+        try {
+            $linfo = new Linfo($this->config);
+
+            return new LinfoWrapper($linfo);
+        } catch (FatalException $exception) {
+            $windows = new WindowsOs();
+
+            return new WindowsWrapper($windows);
+        }
+    }
+}
